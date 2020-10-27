ec700d959b13e811b89c48842a40a54dacd3e311
Mahdi Al Housseini <mahdi.alhousseini@cogentsoftech.com>
2020-10-27 14:45:15 +0200

    updated image with blur detectiona

--- UploadImage/settings.py
+++ UploadImage/settings.py
@@ -62,7 +62,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR,'reactapp/build')
+            os.path.join(BASE_DIR,'image/templates')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'mediafiles')
@@ -137,4 +137,4 @@ STATICFILES_FINDERS = [
 
 ]
 #TEMP = os.path.join(BASE_DIR, 'temp')
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'reactapp/build/static'),]
+#STATICFILES_DIRS=[os.path.join(BASE_DIR,'reactapp/build/static'),]

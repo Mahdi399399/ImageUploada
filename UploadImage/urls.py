4f7804a5cf3da2336985864a5f56006f65855554
Mahdi Al Housseini <mahdi.alhousseini@cogentsoftech.com>
2020-10-27 14:41:48 +0200

    updated image with blur detection

--- UploadImage/urls.py
+++ UploadImage/urls.py
@@ -21,8 +21,8 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-path('image', include('image.urls')),
-    path('',TemplateView.as_view(template_name='index.html')),
+path('', include('image.urls')),
+    #path('',TemplateView.as_view(template_name='index.html')),
 
 
 ]
